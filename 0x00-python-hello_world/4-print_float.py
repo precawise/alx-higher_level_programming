8052bd07da491b8fa5bf48c7b609dc175e1c5d13
precawise <udofiaprecawise@gmail.com>
2023-06-05 09:15:58 -0700

    commit please python

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number = 3.14159
-print(f"float: {number:.2f}")
+float_number = 3.14159
+print(f"float: {float_number:.2f}")
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "Holberton School"
-print(str + str + str)
-print(str[0.9])
+string = "Holberton School"
+print(string + string + string)
+print(string[0])
