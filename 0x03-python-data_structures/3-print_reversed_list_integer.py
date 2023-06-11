97b476e1f891797d5431d178fbbf57c113fc93f2
precawise <udofiaprecawise@gmail.com>
2023-06-11 12:05:12 -0700

    new replace element

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    new_list = my_list.copy()
-    if idx < 0 or idx > len(my_list) - 1:
-        return new_list
-    else:
-        new_list[idx] = element
-        return new_list
+def print_reversed_list_integer(my_list=[]):
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for item in my_list:
+            print("{:d}".format(item))
