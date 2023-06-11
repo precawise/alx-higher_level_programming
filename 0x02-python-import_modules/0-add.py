1613b477c4ad104f359a923c342e34fe67e2e1de
precawise <udofiaprecawise@gmail.com>
2023-06-11 13:13:43 -0700

    modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from bdd_0 import bdd
+
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, bdd(a, b)), end="")
+    print("{} + {} = {}".format(a, b, bdd(a, b)))
