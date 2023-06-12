068081e42117cac2cabdcc177cd1cd738514dbc8
precawise <udofiaprecawise@gmail.com>
2023-06-12 13:43:45 -0700

    calculate list

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-if name == "main":
-import calculator_1
-a = 10
-b = 5
-
-add_result = calculator_1.add(a, b)
-sub_result = calculator_1.sub(a, b)
-mul_result = calculator_1.mul(a, b)
-div_result = calculator_1.div(a, b)
-
-print(f"{a} + {b} = {add_result}")
-print(f"{a} - {b} = {sub_result}")
-print(f"{a} * {b} = {mul_result}")
-print(f"{a} / {b} = {div_result}")
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    add_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
+    print("{} + {} = {}".format(a, b, add_result))
+    print("{} - {} = {}".format(a, b, sub_result))
+    print("{} * {} = {}".format(a, b, mul_result))
+    print("{} / {} = {}".format(a, b, div_result))
