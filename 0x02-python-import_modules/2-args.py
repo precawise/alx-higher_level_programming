f4027b4ae88f3d0611575b71f20c1171cd619d25
precawise <udofiaprecawise@gmail.com>
2023-06-12 13:59:10 -0700

    commit script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
     if __name__ == "__main__":
     import sys
-    num = len(sys.argv)
 
-    if num == 1:
-    print("{} arguments.".format(num - 1))
-    elif num == 2:
-    print("{} argument:".format(num - 1))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-    print("{} arguments:".format(num - 1))
-
-    for i in range(0, num - 2):
-    print("{}: {}".format(i + 2, sys.argv[i + 2]))
+        print("{} arguments:".format(count))
+    for j in range(count):
+         print("{}: {}".format(j + 1, sys.argv[j + 1]))
