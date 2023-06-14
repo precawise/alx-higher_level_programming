9fd426bde4011614904bae2770ec5ef95d6f0913
precawise <udofiaprecawise@gmail.com>
2023-06-14 14:49:27 -0700

    infinite add

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the sum, difference, multiple and quotient of 10 and 5."""
-    from calculator_1 import add, sub, mul, div
-
+    import calculator_1 as calc
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    add_result = calc.add(a, b)
+    sub_result = calc.sub(a, b)
+    mul_result = calc.mul(a, b)
+    div_result = calc.div(a, b)
+
+        print("{} + {} = {}".format(a, b, add_result))
+        print("{} - {} = {}".format(a, b, sub_result))
+        print("{} * {} = {}".format(a, b, mul_result))
+        print("{} / {} = {}".format(a, b, div_result))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
     import sys
 
-    value = 0
+    total = 0
     for i in range(len(sys.argv) - 1):
-        value = value + int(sys.argv[i + 1])
-    print("{}".format(value))
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
