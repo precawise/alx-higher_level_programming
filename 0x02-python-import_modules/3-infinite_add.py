ebba7dfd994756937fb7ff1f9f0d8036f6240ca3
precawise <udofiaprecawise@gmail.com>
2023-06-14 15:29:20 -0700

    5-variable_load.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
     import sys
 
     total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
+    
+    if len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
+    
+    print("Sum: {}".format(total))
