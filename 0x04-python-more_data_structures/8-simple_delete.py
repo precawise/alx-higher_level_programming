18c5af1a12328153bb51eeff12b58f7c6a28881d
precawise <udofiaprecawise@gmail.com>
2023-06-16 14:11:46 -0700

    delete dictionary

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    new_dictionary = dict(a_dictionary)
+    if key in new_dictionary:
+        del new_dictionary[key]
+    return new_dictionary
