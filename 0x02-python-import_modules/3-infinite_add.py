6a83ce9326cce156903d3440aeaf3f81293e204f
precawise <udofiaprecawise@gmail.com>
2023-08-11 18:21:33 +0000

    commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,15 +4,7 @@ if __name__ == "__main__":
     import sys
 
     total = 0
+
     for i in range(1, len(sys.argv)):
-        total += int(sys.argv[i])
-    
-    if len(sys.argv) == 2:
-        print("{} argument:".format(len(sys.argv) - 1))
-    else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-    
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
-    
-    print("Sum: {}".format(total))
+	sum += int(sys.argv[i]) 
+    print("{}".format(total))
