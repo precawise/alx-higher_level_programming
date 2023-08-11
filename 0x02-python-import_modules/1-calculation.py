a478e8066727329c5e206e2e56441f4de7e161f3
precawise <udofiaprecawise@gmail.com>
2023-08-11 19:55:16 +0000

    commit all

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import result_add, result_sub, result_mul, result_div
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, result_add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, result_sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b,result_mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, result_div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import result_add, result_sub, result_mul, result_div
+    from calculator_1 import add, sub, mul, div
     import sys
-    num_args = len(sys.argv)
-    if num_args != 4:
+    arg = len(sys.argv) - 1
+    if arg != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
+
+    ops = sys.argv[2]
     a = int(sys.argv[1])
-    op = sys.argv[2]
     b = int(sys.argv[3])
-    if op == '+':
-        print("{} {} {} = {}".format(a, op, b, result_add(a, b)))
-    elif op == '-':
-        print("{} {} {} = {}".format(a, op, b, result_sub(a, b)))
-    elif op == '*':
-        print("{} {} {} = {}".format(a, op, b, result_mul(a, b)))
-    elif op == '/':
-        print("{} {} {} = {}".format(a, op, b, result_div(a, b)))
+
+    if ops == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, ops, b, add(a, b)))
+    elif ops == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, ops, b, sub(a, b)))
+    elif ops == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, ops, b, div(a, b)))
+    elif ops == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, ops, b, mul(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1):wq
+
