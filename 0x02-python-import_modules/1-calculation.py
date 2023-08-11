4834001d0641bcbed6aacfb372fee4d40d1c9d4e
precawise <udofiaprecawise@gmail.com>
2023-08-11 17:58:14 +0000

    calculate one

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import calculator_1 as calc
-    a = 10
-    b = 5
-
-    add_result = calc.add(a, b)
-    sub_result = calc.sub(a, b)
-    mul_result = calc.mul(a, b)
-    div_result = calc.div(a, b)
-
-        print("{} + {} = {}".format(a, b, add_result))
-        print("{} - {} = {}".format(a, b, sub_result))
-        print("{} * {} = {}".format(a, b, mul_result))
-        print("{} / {} = {}".format(a, b, div_result))
+	from calculator_1 import add_result, sub_result, mul_result, div
+	a = 10
+	b = 5
+        print("{:d} + {:d} = {:d}".format(a, b, add_result))
+        print("{:d} - {:d} = {:d}".format(a, b, sub_result))
+        print("{:d} * {:d} = {:d}".format(a, b, mul_result))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b))
