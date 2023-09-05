9dcf63150952e609312285e5a7d3da01104f9135
precawise <udofiaprecawise@gmail.com>
2023-09-05 13:25:57 +0000

    commit

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    4-print_square Module
+"""
+
+
+def print_square(size):
+    """
+        Prints a square with the character '#'
+
+        Args:
+            size: size length of the square
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for r in range(size):
+        for p in range(size):
+            print("#", end='')
+        print()
